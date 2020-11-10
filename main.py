3044ddb5e958044375bf5200967498c7b8ee81d7
SAVL <salexandervl@gmail.com>
2020-11-10 09:44:33 +0300

    first project python 1.0

---
+++ main.py
@@ -0,0 +1,27 @@
+# number example
+age = 19  # integer
+print(age, type(age))
+mounth = 6.5  # float
+print(mounth, type(mounth), mounth + age, mounth - age, -mounth, mounth * age, abs(-mounth), age ** 5)
+days = 129  # integer
+print(days, type(days), age / mounth, age // mounth, age % mounth, divmod(age, mounth), pow(days, 3))
+# text example
+male = 'female'
+print(male, type(male))
+education = 'forward'
+print(education, type(education))
+married = 'yes'
+print(married, type(married))
+# difficult
+bro = [1, 23, 'text', [10, 2020]]
+print(bro)  # 1
+adress = {
+    'republic': 'Belarus',
+    'city': 'Baranovichy',
+    'street': 'Fedko',
+    'home': 5,
+}
+print(adress)  # 2
+telephone = (1, 2, 'San', 90, 0.7)
+no_telephone = {1, 2, 'San', 90, 0.7}
+print(telephone, no_telephone)
